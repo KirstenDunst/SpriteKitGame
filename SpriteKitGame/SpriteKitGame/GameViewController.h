a5c3f4e96178d11494cf1a1bc541237036e46e94
KirstenDunst <cao_shixin@yahoo.com>
2017-09-14 14:04:43 +0800

    最初的spritekit样式提交，工程创建。

---
+++ SpriteKitGame/SpriteKitGame/GameViewController.h
@@ -0,0 +1,15 @@
+//
+//  GameViewController.h
+//  SpriteKitGame
+//
+//  Created by CSX on 2017/9/14.
+//  Copyright © 2017年 宗盛商业. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+#import <GameplayKit/GameplayKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
---
+++ SpriteKitGame/SpriteKitGame/GameViewController.m
@@ -0,0 +1,63 @@
+//
+//  GameViewController.m
+//  SpriteKitGame
+//
+//  Created by CSX on 2017/9/14.
+//  Copyright © 2017年 宗盛商业. All rights reserved.
+//
+
+#import "GameViewController.h"
+#import "GameScene.h"
+
+@implementation GameViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
+    // including entities and graphs.
+    GKScene *scene = [GKScene sceneWithFileNamed:@"GameScene"];
+    
+    // Get the SKScene from the loaded GKScene
+    GameScene *sceneNode = (GameScene *)scene.rootNode;
+    
+    // Copy gameplay related content over to the scene
+    sceneNode.entities = [scene.entities mutableCopy];
+    sceneNode.graphs = [scene.graphs mutableCopy];
+    
+    // Set the scale mode to scale to fit the window
+    sceneNode.scaleMode = SKSceneScaleModeAspectFill;
+    
+    SKView *skView = (SKView *)self.view;
+    
+    // Present the scene
+    [skView presentScene:sceneNode];
+    
+    skView.showsFPS = YES;
+    skView.showsNodeCount = YES;
+}
+
+//允许设备自动调整横竖设备的状体
+- (BOOL)shouldAutorotate {
+    return YES;
+}
+//根据设备的状态调整状态的旋转样式。
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations {
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        return UIInterfaceOrientationMaskAllButUpsideDown;
+    } else {
+        return UIInterfaceOrientationMaskAll;
+    }
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+
+//隐藏状态栏
+- (BOOL)prefersStatusBarHidden {
+    return YES;
+}
+
+@end
