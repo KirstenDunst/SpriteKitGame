2f90c4da778da41ae5273bc97bc07d0a9bbb5320
KirstenDunst <cao_shixin@yahoo.com>
2017-09-22 16:42:19 +0800

    添加转换成重用的思想。（虽然对于这个游戏点击会取消掉，没有什么作用）

--- SpriteKitGame/SpriteKitGame/StampBlackBlockScene.m
+++ SpriteKitGame/SpriteKitGame/StampBlackBlockScene.m
@@ -18,11 +18,26 @@
 }
 @property (nonatomic) NSTimeInterval lastSpawnTimeInterval;
 @property (nonatomic) NSTimeInterval lastUpdateTimeInterval;
+@property (nonatomic, strong)NSMutableArray *dataArr;
+
+@property (nonatomic, strong)NSMutableArray *actionArr;
 
 @end
 
 @implementation StampBlackBlockScene
 
+- (NSMutableArray *)actionArr{
+    if (!_actionArr) {
+        _actionArr = [NSMutableArray array];
+    }
+    return _actionArr;
+}
+- (NSMutableArray *)dataArr{
+    if (!_dataArr) {
+        _dataArr = [NSMutableArray array];
+    }
+    return _dataArr;
+}
 - (instancetype)initWithSize:(CGSize)size{
     self = [super initWithSize:size];
     if (self) {
@@ -41,24 +56,35 @@
         return;
     }
     int a = arc4random()%4;
-    SKSpriteNode*whiteNode = [SKSpriteNode spriteNodeWithColor:[UIColor whiteColor] size:CGSizeMake(self.view.frame.size.width/4, self.frame.size.height/5)];
+    
+    SKSpriteNode *whiteNode;
+    if (self.dataArr.count>0) {
+        whiteNode = self.dataArr[0];
+        [self.dataArr removeObjectAtIndex:0];
+    }else{
+        whiteNode = [SKSpriteNode spriteNodeWithColor:[UIColor whiteColor] size:CGSizeMake(self.view.frame.size.width/4, self.frame.size.height/5)];
+        [self addChild:whiteNode];
+    }
     whiteNode.position = CGPointMake(self.view.frame.size.width/8+(self.view.frame.size.width/4)*a, self.frame.size.height+self.frame.size.height/10);
     whiteNode.name = [NSString stringWithFormat:@"heroNode%d",index];
-    [self addChild:whiteNode];
+   
 //    时间限制保证下落的速度能够和白块之间连接起来
-    SKAction *actionMove = [SKAction moveTo:CGPointMake(self.view.frame.size.width/8+(self.view.frame.size.width/4)*a, -self.frame.size.height/10) duration:6*speed];
+    SKAction *actionMove = [SKAction moveTo:CGPointMake(whiteNode.position.x, -self.frame.size.height/10) duration:6*speed];
     SKAction *loseAction = [SKAction runBlock:^{
     //        反转动画效果时间设置
         SKTransition *reveal = [SKTransition flipVerticalWithDuration:0.5];
         SKScene *gameOverScene = [[GameOverScene alloc] initWithSize:self.size won:NO];
         [self.view presentScene:gameOverScene transition:reveal];
     }];
-    SKAction *actionDone = [SKAction removeFromParent];
+    SKAction *actionDone = [SKAction runBlock:^{
+        [self.dataArr addObject:whiteNode];
+    }];
     
     [whiteNode runAction:[SKAction sequence:@[actionMove,loseAction,actionDone]]];
 }
 
 // 获取时间增量
+//姐，你家里面的地址给我发一个吧，我想给你和俺大姨带点这边的特产邮回去。主要是这次要先到可可家里一趟，我带这些不方便，要不是我都自己带回去了。
 // 如果我们运行的每秒帧数低于60，我们依然希望一切和每秒60帧移动的位移相同
 - (void)update:(NSTimeInterval)currentTime{
     NSLog(@">>>>>>>>>>>>>%f",currentTime);
